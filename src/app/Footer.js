43de916098715c2de6a0ff280867d945bbc34c70
batanashan <shanmukhaciv@gmail.com>
2023-09-26 07:50:20 +0530

    files added

---
+++ src/app/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='bg-dark position-fixed bottom-0 w-100 text-white text-center footer'>Footer</div>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,4 @@
+import Add from "./add/page";
+
+export default Add;
+
